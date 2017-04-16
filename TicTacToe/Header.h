6804a682a620e336c43cc1076a7a46b70f52bf75
Dawid Wilk <d.wilk.1992@gmail.com>
2017-04-16 18:14:51 +0200

    Update Header.h

--- TicTacToe/Header.h
+++ TicTacToe/Header.h
@@ -1,10 +1,10 @@
 #pragma once
 
-void drawGameBoard(char *cBoard);						//Prepares game board
+void drawGameBoard(char *cBoard);						    //Prepares game board
 bool checkWin(char *cBoard, char symbol);				//return true if player wins
-bool deadHeat(char *cBoard);							//Return true if nobody wins
+bool deadHeat(char *cBoard);							      //Return true if nobody wins
 void round(char *cBoard, char &symbol);					//Player round
 int minMax(char *cBoard, char symbol);					//MinMax recursion algorithm
-int computerMove(char *cBoard);							//Computer best move
+int computerMove(char *cBoard);							    //Computer best move
 
-extern char cSymbol;									//Game symbol 'X' or 'O'
+extern char cSymbol;									          //Game symbol 'X' or 'O'
